83f550ce848a4635a770094d7161984bf8f8508e
Ryouji <1354413904@qq.com>
2022-01-17 21:07:17 +0800

    feat:Interface permission verification using Interceptor

---
+++ src/main/java/com/stelpolvo/wiki/interceptor/AuthInterceptor.java
@@ -0,0 +1,44 @@
+package com.stelpolvo.wiki.interceptor;
+
+import com.alibaba.fastjson.JSON;
+import com.stelpolvo.wiki.domain.vo.UserVo;
+import com.stelpolvo.wiki.exception.UserException;
+import com.stelpolvo.wiki.utils.UserContext;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+    @Resource
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // OPTIONS请求不做校验
+        if (request.getMethod().toUpperCase(Locale.ROOT).equals("OPTIONS")) {
+            return true;
+        }
+        String token = request.getHeader("token");
+        if (ObjectUtils.isEmpty(token)) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            throw new UserException("请先登录");
+        }
+        String o = redisTemplate.opsForValue().get(token);
+        if (o == null) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            throw new UserException("请先登录");
+        } else {
+            UserContext.setUser(JSON.parseObject(o, UserVo.class));
+            return true;
+        }
+    }
+}
